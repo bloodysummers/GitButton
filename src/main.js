c79f06e9373b0401193b1309df51f2d7ab9adffa
Luis Fernando Díaz <bloodysummers@gmail.com>
2018-02-14 00:24:20 -0600

    Adjustments in mainWindow

--- src/main.js
+++ src/main.js
@@ -3,16 +3,18 @@ const path = require('path')
 
 const { app, BrowserWindow, Menu, ipcMain: ipc } = electron
 
-// require('electron-reload')(__dirname, {
-//     electron: path.join(__dirname, 'node_modules', '.bin', 'electron')
-// });
+require('electron-reload')(__dirname, {
+    electron: path.join(__dirname, 'node_modules', '.bin', 'electron')
+});
 
 let mainWindow
 
 app.on('ready', () => {
     mainWindow = new BrowserWindow({
-        width:250,
-        height:400
+        width:750,
+        height:500,
+        resizable: false,
+        frame: false
     })
 
     mainWindow.loadURL(path.join(__dirname, 'index.html'))
