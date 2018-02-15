bb4833bd0e1cd98fe829e5707d9e87537775361a
Luis Fernando Díaz <bloodysummers@gmail.com>
2018-02-15 12:04:42 -0600

    Serial communication is now in comm.js file

--- src/renderers/index/comm.js
+++ src/renderers/index/comm.js
@@ -1,17 +1,43 @@
 const electron = require('electron')
+const SerialPort = require('serialport')
 
 const { ipcRenderer: ipc } = electron
+const { Readline } = SerialPort.parsers
 
-const button = document.getElementById('button')
+SerialPort.list((err, ports) => {
+    console.log(ports)
+})
 
 let commitAvailable = true
 
+// UI Button
+const button = document.getElementById('button')
 button.addEventListener('click', () => {
-    button.classList.add('pushed')
-    if (commitAvailable)
+    if (commitAvailable) {
+        button.classList.add('pushed')
         ipc.send('showCommit')
+    }
 })
 
+// Serial Button
+const port = new SerialPort('COM3', {
+    baudRate: 9600
+})
+const parser = port.pipe(new Readline({ delimiter: '\r\n' }))
+parser.on('data', (data) => {
+    console.log(data)
+    if (commitAvailable && data == "Commit!") {
+        button.classList.add('pushed')
+        ipc.send('showCommit')
+    }
+})
+port.on('open', () => {
+})
+port.on('error', function(error){
+    console.log(error)
+})
+
+// Communication
 ipc.on('blockComm', () => {
     commitAvailable = false
 })
