fff1ab533a0a187610efe530f2ea8aabca6d86ac
Luis Fernando Díaz <bloodysummers@gmail.com>
2018-02-15 23:26:52 -0600

    Added functions to get current directory or delete a project

--- src/renderers/index/repo.js
+++ src/renderers/index/repo.js
@@ -1,13 +1,19 @@
 const fs = require('fs')
 const exec = require('child_process').exec
 const os = require('os')
-const { addRepository, getRepositories } = require('./store')
+const { addRepository, getRepositories, setSelectedProject, getSelectedProject, hashExists, getCurrentDirectory, deleteRepository } = require('./store')
 
+const repoWindow = $('.repo-container')
 const errorMessage = $('#repo-error')
 const inputFile = $('#search-repo')
 const inputFileButton = $('#search-repo-button')
 const addRepoButton = $('#add-repo-button')
 const repoList = $('#repo-list')
+const repositoryButton = $('#repository')
+
+let directory
+let project
+let showRepoSettings = false
 
 inputFileButton.on('click', () => {
     inputFile.click()
@@ -30,9 +36,15 @@ function checkGitStatus(dir) {
         else if (/nothing to commit/.test(stdout)) status = 'clean'
         else status = 'dirty'
         if (status != 'unknown') {
-            errorMessage.text('')
-            addRepository(dir)
-            listRepositories()
+            exec(`echo "${dir}" | git hash-object --stdin`, (err, hash) => {
+                if (!hashExists(hash.trim())) {
+                    errorMessage.text('')
+                    addRepository(dir, hash.trim())
+                    listRepositories()
+                } else {
+                    errorMessage.text('Already stored')
+                }
+            })
         } else {
             errorMessage.text('Not a git repository')
         }
@@ -53,15 +65,53 @@ inputFile.on('change', () => {
     }
 })
 
+repoList.on('click', '.repo-item', function() {
+    let hash = $(this).data('hash')
+    setSelectedProject(hash)
+    listRepositories()
+})
+repoList.on('click', '.delete-project', function(e) {
+    e.stopPropagation()
+    let hash = $(this).parent().data('hash')
+    deleteRepository(hash)
+    listRepositories()
+})
+repositoryButton.on('click', (e) => {
+    console.log(e)
+    if (showRepoSettings) {
+        repoWindow.fadeOut(300)
+        showRepoSettings = false
+    } else {
+        repoWindow.fadeIn(300)
+        showRepoSettings = true
+    }
+})
+repoWindow.find('.close-icon').on('click', () => {
+    repoWindow.fadeOut(300)
+    showRepoSettings = false
+})
+
 function listRepositories() {
     const repos = getRepositories()
+    let selected = getSelectedProject()
     repoList.html('')
     for (let i = 0; i < repos.length; i++) {
-        repoList.append(`<div class="repo-item">
-            <h4 class="repo-name">${repos[i].name}</h4>
-            <p className="repo-location">${repos[i].location}</p>
-        </div>`)
+        let hash = repos[i].hash
+        let active = hash == selected
+        repoList.append(`
+            <div class="repo-item${active ? ' active' : ''}" data-hash=${repos[i].hash}>
+                <div class="delete-project"><i class="fa fa-trash"></i></div>
+                <h4 class="repo-name">${repos[i].name}</h4>
+                <p className="repo-location">${repos[i].location}</p>
+            </div>`
+        )
     }
+    directory = getCurrentDirectory(selected)
+    project = getCurrentDirectory(selected, true)
+    if (project)
+        repositoryButton.find('.title').text(project)
+    else
+        repositoryButton.find('.title').text('No project selected')
 }
 
 listRepositories()
--- src/renderers/index/store.js
+++ src/renderers/index/store.js
@@ -3,6 +3,9 @@ const os = require('os')
 
 const store = new Store()
 
+// store.clear()
+console.log(store.store)
+
 if (!store.get('repositories')) {
     store.set('repositories', [])
 }
@@ -11,16 +14,70 @@ exports.getRepositories = () => {
     return (store.get('repositories'))
 }
 
-exports.addRepository = (dir) => {
+exports.hashExists = (hash) => {
     let repositories = store.get('repositories')
-    let index = dir.lastIndexOf('/')
+    for (let i=0; i<repositories.length; i++) {
+        if (hash == repositories[i].hash)
+            return true
+    }
+    return false
+}
+
+exports.addRepository = (dir, hash) => {
+    let repositories = store.get('repositories')
     let location, name
+    let index = dir.lastIndexOf('/')
     if (index == -1)
         index = dir.lastIndexOf('\\')
     location = dir.slice(0, index)
     name = dir.slice(index + 1)
     repositories.push({
-        location, name
+        location, name, hash, dir
     })
+    repositories.sort(function(a, b) {
+        var projectA = a.name.toUpperCase();
+        var projectB = b.name.toUpperCase();
+        return (projectA < projectB) ? -1 : (projectA > projectB) ? 1 : 0;
+    });
     store.set('repositories', repositories)
+    store.set('project', hash)
+}
+
+exports.deleteRepository = (hash) => {
+    let repositories = store.get('repositories')
+    let index
+    let repository = repositories.map((repo, i) => {
+        if (repo.hash == hash)
+            index = i
+    })
+    repositories.splice(index, 1)
+    store.set('repositories', repositories)
+    if (repositories.length) {
+        if (index == 0)
+            store.set('project', repositories[0].hash)
+        else
+            store.set('project', repositories[index-1].hash)
+    }
+
+}
+
+exports.getCurrentDirectory = (hash, getName) => {
+    let repositories = store.get('repositories')
+    if (repositories.length) {
+        let repo = repositories.filter(repo => {
+            return repo.hash == hash
+        })
+        if (getName)
+            return repo[0].name
+        else
+            return repo[0].dir
+    }
+}
+
+exports.setSelectedProject = (hash) => {
+    store.set('project', hash)
+}
+
+exports.getSelectedProject = () => {
+    return store.get('project')
 }
